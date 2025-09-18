7eba7891c2a4ef773c1fe0a5911a31aeb2d9b2de
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-09-18 14:54:05 +0530

    updateq

--- app/products/rings/page.jsx
+++ app/products/rings/page.jsx
@@ -151,7 +151,7 @@ export default function CategoryPage() {
       {/* Hero Section */}
       <section
         className='relative w-full h-screen flex items-center justify-center bg-fixed bg-center bg-cover'
-        style={{ backgroundImage: "url('../ringbgc.png')" }}
+        style={{ backgroundImage: "url('../ringbg.png')" }}
       >
         {/* Glassmorphism Overlay */}
         <div className='absolute inset-0 bg-black/10 backdrop-blur-sm border border-white/20 shadow-inner' />
